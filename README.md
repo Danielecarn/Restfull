dda50d817e83cfda64de4ce24cd385ec2a435a4f
Danielecarn <dannygoncalves41@gmail.com>
2022-12-03 16:32:15 -0300

    feat: Add all data request

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ No diretório do projeto, você pode executar:
 Para executar o servidor:
 
 ### `node server.js`
+
+Para acessar todos os clientes no navegador:
+
+### `http://localhost:3000/clients`
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const app = express();
+const data = require("./data.json");
 
-const app = express()
+app.use(express.json());
 
-app.get("/clients", function(req, res){});
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+
 app.get("/clients/:id", function(req, res){});
 app.post("/clients", function(req, res){});
 app.put("/clients/:id", function(req, res){});
